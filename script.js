8b1e96bb2fd34cc88396d1088a9297e1b4ae340c
jecrozzet <jecrozzet@gmail.com>
2022-09-13 13:02:21 +0800

    Edit script.js to add function game

--- script.js
+++ script.js
@@ -14,7 +14,10 @@ function getPlayerChoice(){
     let keepGoing = true;
 
     while (keepGoing){
-        let choice = prompt("Rock\/Paper\/Scissors").toLowerCase();
+        let choice = prompt("Rock\/Paper\/Scissors");
+        if (choice){
+            choice = choice.toLowerCase();
+        }
 
         if (choice === "rock") {
             return "Rock";
@@ -34,29 +37,54 @@ function playRound(playerChoice, computerChoice){
             case "Rock":
                 return "Draw! Rock equals Rock";
             case "Paper":
-                return "Lose! Rock covered by Paper";
+                computerScore++;
+                return "You Lose! Rock covered by Paper";
             case "Scissors":
-                return " Win! Rock beats Scissors";
+                playerScore++;
+                return "You Win! Rock beats Scissors";
         }
     } else if (playerChoice === "Paper") {
         switch (computerChoice) {
             case "Rock":
-                return " Win! Paper covers Rock";
+                playerScore++;
+                return "You Win! Paper covers Rock";
             case "Paper":
                 return "Draw! Paper equals Paper";
             case "Scissors":
-                return "Lose! Paper cut by Scissors";
+                computerScore++;
+                return "You Lose! Paper cut by Scissors";
         }
     } else if (playerChoice === "Scissors") {
         switch (computerChoice) {
             case "Rock":
-                return "Lose! Scissors beaten by Rock";
+                computerScore++;
+                return "You Lose! Scissors beaten by Rock";
             case "Paper":
-                return " Win! Scissors cuts Paper";
+                playerScore++;
+                return "You Win! Scissors cuts Paper";
             case "Scissors":
                 return "Draw! Scissors equals Scissors";
         }
     }
 }
 
-console.log(playRound(getPlayerChoice(), getComputerChoice()));
+function game() {
+    for(let i = 0; i < 5 ; i++) {
+        alert("Round " + (i + 1));
+        alert(playRound(getPlayerChoice(), getComputerChoice()));
+    }
+
+    alert("Your Score: " + playerScore + ", Computer Score: " + computerScore);
+
+    if (playerScore > computerScore) {
+        alert("You Win!");
+    } else if (playerScore < computerScore) {
+        alert("You Lose!");
+    } else {
+        alert("Draw!");
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+game();
